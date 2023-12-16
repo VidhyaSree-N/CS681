a32d98fe0af4a213619d031555d980c944426328
unknown <vidhyasreen003@gmail.com>
2023-12-16 14:44:58 -0500

    HW12

---
+++ HW12/src/edu/umb/cs681/hw12/DepositRunnable.java
@@ -0,0 +1,33 @@
+package edu.umb.cs681.hw12;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class DepositRunnable implements Runnable {
+
+    private BankAccount bankAccount;
+    private AtomicBoolean done = new AtomicBoolean(false);
+
+    public DepositRunnable(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public void setDone(){
+        done.set(true);
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            try {
+            if(done.get()){
+                System.out.println("Done flag set , Terminating Deposit thread");
+                break;
+            }
+            bankAccount.deposit(100);
+            Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
