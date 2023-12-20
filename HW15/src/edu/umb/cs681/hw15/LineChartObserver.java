a39cfd2dab57e462c97513d75b65ce49d691cad5
unknown <vidhyasreen003@gmail.com>
2023-12-19 22:02:09 -0500

    HW15

---
+++ HW15/src/edu/umb/cs681/hw15/LineChartObserver.java
@@ -0,0 +1,12 @@
+package edu.umb.cs681.hw15;
+
+public class LineChartObserver implements Observer<StockEvent> {
+
+    @Override
+    public void update(Observable<StockEvent> sender, StockEvent event) {
+        String ticker = event.ticker();
+        double quote = event.quote();
+        System.out.println("LineChartObserver : ticker = " + ticker + " and quote = "+ quote);
+    }
+
+}
---
+++ HW15/src/edu/umb/cs681/hw15/ThreeDObserver.java
@@ -0,0 +1,10 @@
+package edu.umb.cs681.hw15;
+
+public class ThreeDObserver implements Observer<StockEvent> {
+    @Override
+    public void update(Observable<StockEvent> sender, StockEvent event) {
+        String ticker = event.ticker();
+        double quote = event.quote();
+        System.out.println("3DObserver : ticker = " + ticker + " and quote = "+ quote);
+    }
+}
