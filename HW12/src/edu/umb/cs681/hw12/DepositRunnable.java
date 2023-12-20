f8d7c2b5bbe1437af1523cb4cac20a12b808f2e0
unknown <vidhyasreen003@gmail.com>
2023-12-19 21:59:20 -0500

    Modifications

--- HW12/src/edu/umb/cs681/hw12/DepositRunnable.java
+++ HW12/src/edu/umb/cs681/hw12/DepositRunnable.java
@@ -1,11 +1,13 @@
 package edu.umb.cs681.hw12;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DepositRunnable implements Runnable {
 
     private BankAccount bankAccount;
     private AtomicBoolean done = new AtomicBoolean(false);
+    private ReentrantLock lock = new ReentrantLock();
 
     public DepositRunnable(BankAccount bankAccount) {
         this.bankAccount = bankAccount;
@@ -18,6 +20,7 @@ public class DepositRunnable implements Runnable {
     @Override
     public void run() {
         while (true){
+            lock.lock();
             try {
             if(done.get()){
                 System.out.println("Done flag set , Terminating Deposit thread");
@@ -27,6 +30,9 @@ public class DepositRunnable implements Runnable {
             Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            }
+            finally {
+                lock.unlock();
             }
         }
     }
--- HW12/src/edu/umb/cs681/hw12/WithdrawRunnable.java
+++ HW12/src/edu/umb/cs681/hw12/WithdrawRunnable.java
@@ -1,11 +1,13 @@
 package edu.umb.cs681.hw12;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class WithdrawRunnable implements Runnable {
 
     private BankAccount bankAccount;
     private AtomicBoolean done = new AtomicBoolean(false);
+    private ReentrantLock lock = new ReentrantLock();
 
     public WithdrawRunnable(BankAccount bankAccount) {
         this.bankAccount = bankAccount;
@@ -18,6 +20,7 @@ public class WithdrawRunnable implements Runnable {
     @Override
     public void run() {
         while (true){
+            lock.lock();
             try {
             if(done.get()){
                 System.out.println("Done flag set, Terminating withdraw thread.");
@@ -27,6 +30,9 @@ public class WithdrawRunnable implements Runnable {
             Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            }
+            finally {
+                lock.unlock();
             }
         }
     }
